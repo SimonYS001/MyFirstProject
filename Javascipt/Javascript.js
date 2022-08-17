c30856768779dbad167e618ddfd5c64e96657dfe
Simon_Yang <su.simon.yang@gmail.com>
2022-08-16 21:53:22 -0700

    good

--- Javascipt/Javascript.js
+++ Javascipt/Javascript.js
@@ -1,32 +1,10 @@
-"use strict";
+function checkCashRegister(price, cash, cid) {
+    const INSUFFICIENT_FUNDS = { status: "INSUFFICIENT_FUNDS", change: [] }
+    const CLOSED = { status: "CLOSED", change: cid }
+    const OPEN = { status: "OPEN", change: [] }
 
-// let ys = {
-//     name: 'yangsu',
-//     show: function () {
-//         console.log(this.name);
-//     }
-// }
+}
 
-// let dn = {
-//     name: 'dongning',
-//     show: function () {
-//         console.log(this.name);
-//     }
-// }
+let ss = checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
 
-
-
-
-function user(name) {
-    return {
-        name,
-        show: function () {
-            console.log(this.name);
-        }
-    }
-};
-
-let ys = user("yangsu")
-ys.show()
-console.log(ys);
-
+console.log(ss);
